dc024a5bd6e5e9678314693b390911ff847b9151
hernandck1 <hernandck@msn.com>
2023-01-18 22:51:20 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pragma.hernancatacoly.thecats',
+  appName: 'pragmaTheCats',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
